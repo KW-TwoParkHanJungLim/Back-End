f70b171dbd531dc41da8663e46a9c1200a2b2246
HanSeoJin <yp6484@gmail.com>
2022-09-25 21:13:06 +0900

    Add Project file

---
+++ src/test/java/com/awexomeray/TwoParkHanJungLim/TwoParkHanJungLimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awexomeray.TwoParkHanJungLim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwoParkHanJungLimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
