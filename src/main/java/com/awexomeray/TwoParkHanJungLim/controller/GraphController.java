b168c328132d38c5a422540b2521876b2bf4da61
HanSeoJin <yp6484@gmail.com>
2022-12-27 04:01:41 +0900

    [수정] repository 위치 변경

--- src/main/java/com/awexomeray/TwoParkHanJungLim/controller/GraphController.java
+++ src/main/java/com/awexomeray/TwoParkHanJungLim/controller/GraphController.java
@@ -1,7 +1,7 @@
 package com.awexomeray.TwoParkHanJungLim.controller;
 
 import com.awexomeray.TwoParkHanJungLim.model.GraphModel;
-import com.awexomeray.TwoParkHanJungLim.model.GraphRepository;
+import com.awexomeray.TwoParkHanJungLim.repository.GraphRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
