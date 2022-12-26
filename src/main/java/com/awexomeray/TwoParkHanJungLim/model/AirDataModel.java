63760d2f6a5b6be11f94ad1fe3beacfeb228f2ad
HanSeoJin <yp6484@gmail.com>
2022-12-26 20:48:32 +0900

    [수정] package명 수정

--- src/main/java/com/awexomeray/TwoParkHanJungLim/model/AirDataModel.java
+++ src/main/java/com/awexomeray/TwoParkHanJungLim/model/AirDataModel.java
@@ -1,10 +1,9 @@
-package com.awexomeray.TwoParkHanJungLim.testMVC.model;
+package com.awexomeray.TwoParkHanJungLim.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
 @Builder
 @Getter
 @AllArgsConstructor
