9afecb92987d6cef8def07c546148956a3f71744
kangholim <55732952+Icln@users.noreply.github.com>
2023-01-06 01:29:41 +0900

    [추가] 센서 데이터 DAO 구현

--- src/main/java/com/awexomeray/TwoParkHanJungLim/dao/AirDataDao.java
+++ src/main/java/com/awexomeray/TwoParkHanJungLim/dao/AirDataDao.java
@@ -1,5 +1,6 @@
 package com.awexomeray.TwoParkHanJungLim.dao;
 
+import com.awexomeray.TwoParkHanJungLim.dto.graphDto.RequestGraphDataDto;
 import com.awexomeray.TwoParkHanJungLim.entity.AirDataEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
@@ -9,12 +10,12 @@ import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Map;
 
 @Repository
 public class AirDataDao {
     @Autowired
     private MongoTemplate mongoTemplate;
-
 
     public AirDataEntity findLatestRecordAirData(String collectionName) {
         return mongoTemplate.findOne(
@@ -29,6 +30,17 @@ public class AirDataDao {
                 Query.query(Criteria.where("logtime").is(latestRecordDateTime)),
                 AirDataEntity.class,
                 collectionName
+        );
+    }
+
+    public Map findSensorData(RequestGraphDataDto requestGraphDataDto, String sensorId){
+        Query query = new Query(new Criteria("s_id").all(sensorId));
+        query.addCriteria(new Criteria("logtime").regex(requestGraphDataDto.getLogTime()));
+        query.fields().include("s_id").include("logtime").include(requestGraphDataDto.getAirData());
+        return mongoTemplate.findOne(
+                query.with(Sort.by(Sort.Direction.DESC,"logtime")),
+                Map.class,
+                requestGraphDataDto.getCollection()
         );
     }
 }
