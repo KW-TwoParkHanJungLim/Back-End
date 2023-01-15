1ef2a3b5ca035aa506dd32e9baf44d913a3b38e7
kangholim <55732952+Icln@users.noreply.github.com>
2023-01-15 14:27:49 +0900

    [수정] 코드 리뷰 반영

--- src/main/java/com/awexomeray/TwoParkHanJungLim/dto/graphDto/RequestGraphDataDto.java
+++ src/main/java/com/awexomeray/TwoParkHanJungLim/dto/graphDto/RequestGraphDataDto.java
@@ -10,7 +10,10 @@ import java.util.List;
 @Setter
 public class RequestGraphDataDto {
     private String collection;
+
     private String logTime;
+
     private List<String> sensors;
+
     private String airData;
 }
