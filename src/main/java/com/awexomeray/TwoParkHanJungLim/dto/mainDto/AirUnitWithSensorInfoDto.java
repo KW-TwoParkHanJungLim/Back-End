fdc215617c9ed0420795acf0edf21b8cd5e65dc2
HanSeoJin <87526189+JinSeoHan@users.noreply.github.com>
2023-01-16 18:57:55 +0900

    [수정] 메인화면 API 공기데이터 단위 타입수정 (#18)

--- src/main/java/com/awexomeray/TwoParkHanJungLim/dto/mainDto/AirUnitWithSensorInfoDto.java
+++ src/main/java/com/awexomeray/TwoParkHanJungLim/dto/mainDto/AirUnitWithSensorInfoDto.java
@@ -3,16 +3,25 @@ package com.awexomeray.TwoParkHanJungLim.dto.mainDto;
 import com.awexomeray.TwoParkHanJungLim.unit.AirUnit;
 import lombok.Getter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 public class AirUnitWithSensorInfoDto {
-    private AirUnit unit;
+    private List<AirUnit> unit;
 
     private List<SensorInfoDto> sensorInfoList;
 
     public AirUnitWithSensorInfoDto(List<SensorInfoDto> sensorInfoDtoList) {
-        unit = new AirUnit();
+        unit = new ArrayList<>();
+        unit.add(new AirUnit("Temperature", "ºC"));
+        unit.add(new AirUnit("Humidity", "%"));
+        unit.add(new AirUnit("CO2", "ppm"));
+        unit.add(new AirUnit("TVOC", "ppb"));
+        unit.add(new AirUnit("PM01", "㎍/㎡"));
+        unit.add(new AirUnit("PM2.5", "㎍/㎡"));
+        unit.add(new AirUnit("PM10", "㎍/㎡"));
+
         this.sensorInfoList = sensorInfoDtoList;
     }
 }
