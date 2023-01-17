f91ac0322b9f4d97eba7b163300288885ae8611e
jiwon <83341978+jiione@users.noreply.github.com>
2023-01-17 17:43:02 +0900

    feature#21 유저리스트기능 (#22)
    * 유저리스트기능
    
    * 정렬

---
+++ src/main/java/com/awexomeray/TwoParkHanJungLim/controller/AdminController.java
@@ -0,0 +1,26 @@
+package com.awexomeray.TwoParkHanJungLim.controller;
+
+import com.awexomeray.TwoParkHanJungLim.service.AdminService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+
+    private final AdminService adminService;
+
+
+    @GetMapping("/userList")
+    public ResponseEntity<?> getUserList() {
+        List<String> users = adminService.getAllUserId();
+        return ResponseEntity.ok().body(users);
+    }
+}
