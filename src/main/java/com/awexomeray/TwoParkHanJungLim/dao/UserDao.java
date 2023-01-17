3bc002ed2317bba8366299e66b661c41e8653a85
jiwon <83341978+jiione@users.noreply.github.com>
2023-01-17 18:16:04 +0900

    유저 프로필 기능 구현

--- src/main/java/com/awexomeray/TwoParkHanJungLim/dao/UserDao.java
+++ src/main/java/com/awexomeray/TwoParkHanJungLim/dao/UserDao.java
@@ -3,6 +3,8 @@ package com.awexomeray.TwoParkHanJungLim.dao;
 import com.awexomeray.TwoParkHanJungLim.entity.UserEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
 
 
@@ -15,5 +17,13 @@ public class UserDao {
 
     public List<UserEntity> getUsers() {
         return mongoTemplate.findAll(UserEntity.class, "_users");
+    }
+
+    public UserEntity getUser(String id){
+        return mongoTemplate.findOne(
+                Query.query(Criteria.where("id").is(id)),
+                UserEntity.class,
+                "_users"
+        );
     }
 }
