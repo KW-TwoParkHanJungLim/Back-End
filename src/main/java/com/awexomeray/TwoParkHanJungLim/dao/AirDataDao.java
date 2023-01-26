09729f2339b53f688fd4bd391af6885ceef6d17a
jiwon <83341978+jiione@users.noreply.github.com>
2023-01-26 18:19:21 +0900

    필요없는 코드 제거

--- src/main/java/com/awexomeray/TwoParkHanJungLim/dao/AirDataDao.java
+++ src/main/java/com/awexomeray/TwoParkHanJungLim/dao/AirDataDao.java
@@ -7,7 +7,6 @@ import com.awexomeray.TwoParkHanJungLim.exception.ErrorCodes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.index.Index;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
--- src/main/java/com/awexomeray/TwoParkHanJungLim/entity/AirDataEntity.java
+++ src/main/java/com/awexomeray/TwoParkHanJungLim/entity/AirDataEntity.java
@@ -4,9 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.mongodb.core.index.Indexed;
-import org.springframework.data.mongodb.core.index.TextIndexed;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 @Builder
 @Getter
