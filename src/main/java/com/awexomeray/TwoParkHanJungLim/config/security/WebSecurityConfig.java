097c1da41e68d4aa318d8f28d41230a16218b8f9
jiwon <83341978+jiione@users.noreply.github.com>
2023-01-26 18:38:57 +0900

    로그인 권한 수정 (#31)

--- src/main/java/com/awexomeray/TwoParkHanJungLim/config/security/WebSecurityConfig.java
+++ src/main/java/com/awexomeray/TwoParkHanJungLim/config/security/WebSecurityConfig.java
@@ -28,12 +28,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 토큰 기반 인증이므로 세션 역시 사용하지 않습니다.
                 .and()
                 .authorizeRequests() // 요청에 대한 사용권한 체크
+                .antMatchers("/login/**").permitAll()
                 .antMatchers("/admin/**").hasRole("admin")
-                .antMatchers("/user/**").authenticated()
-                .antMatchers("/details/**").authenticated()
-                .antMatchers("/graph/**").authenticated()
-                .antMatchers("/main/**").authenticated()
-                .anyRequest().permitAll() // 그외 나머지 요청은 누구나 접근 가능
+                .anyRequest().authenticated()// 그외 나머지 요청은 인증하면 접근 가능
                 .and()
                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
                         UsernamePasswordAuthenticationFilter.class)
